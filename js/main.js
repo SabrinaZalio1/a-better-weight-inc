8a4ceea4432159f16913104db1fb7d3f2662d8d2
Sabrina Zalio <sabrinazalio1@gmail.com>
2023-10-05 16:34:27 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+// COUNTDOWN
+function handleCountdown() {
+  const endDate = new Date('2023-10-12 00:00:00').getTime();
+  const now = new Date().getTime();
+  const timeRemaining = endDate - now;
+
+  if (timeRemaining > 0) {
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = String(days).padStart(2, '0');
+    document.getElementById('hours').textContent = String(hours).padStart(2, '0');
+    document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
+    document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
+  } else {
+    document.getElementById('countdown').textContent = 'Countdown expired!';
+  }
+}
+
+function startCountdown() {
+  handleCountdown();
+  setInterval(handleCountdown, 1000);
+}
+
+window.onload = startCountdown;
+
+
+
+// RESPONSIVE NAVBAR AND HAMBURGUER MENU
+
+// Constants
+const hamburgerButton = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".nav-menu");
+
+// Toggle the menu and hamburger button
+function toggleMenu() {
+  hamburgerButton.classList.toggle("active");
+  navLinksContainer.classList.toggle("active");
+}
+
+// Toggle menu on hamburger button click
+hamburgerButton.addEventListener("click", toggleMenu);
+
+// Toggle menu on nav link click (using event delegation)
+navLinksContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("nav-link")) {
+    toggleMenu();
+  }
+});
+
+// Dropdown functionality
+document.addEventListener("click", (e) => {
+  const dropdownButton = e.target.closest("[data-dropdown-button]");
+  const dropdown = e.target.closest("[data-dropdown]");
+
+  if (!dropdownButton && dropdown !== null) return;
+
+  if (dropdownButton) {
+    dropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((otherDropdown) => {
+    if (otherDropdown !== dropdown) {
+      otherDropdown.classList.remove("active");
+    }
+  });
+});
+
+
+
+// ANIMATION OF DATA
+
+const numberElements = document.querySelectorAll('.hero-banner-number');
+
+// Function to animate a number element
+function animateNumber(element) {
+  const initialValue = parseFloat(element.textContent);
+  const targetValue = parseFloat(element.getAttribute('data-count'));
+  const duration = 2000; // Animation duration in milliseconds
+  const startTime = performance.now();
+
+  function updateNumber(timestamp) {
+    const elapsedTime = timestamp - startTime;
+    if (elapsedTime < duration) {
+      // Calculate the current value based on elapsed time and target value
+      const currentValue = initialValue + (targetValue - initialValue) * (elapsedTime / duration);
+      element.textContent = Math.floor(currentValue);
+      requestAnimationFrame(updateNumber);
+    } else {
+      // Animation complete, set the final value
+      element.textContent = Math.floor(targetValue);
+    }
+  }
+
+  // Start the animation
+  requestAnimationFrame(updateNumber);
+}
+
+// Apply animation to each number element
+numberElements.forEach(animateNumber);
